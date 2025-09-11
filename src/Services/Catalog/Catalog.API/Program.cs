ce96f0fef8c102268975c6136e14395f3178c811
Miguel Quiroz <miguel.quiroz@Mac.homenet.telecomitalia.it>
2025-09-11 10:36:45 +0200

    rename tasks

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    // Add explicit return statement to be able to set a breakpoint here
+    Console.WriteLine("Hello World! (from console)");
+    return "Hello World!";
+});
 
 app.Run();
