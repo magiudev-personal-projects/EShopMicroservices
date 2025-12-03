811c921d43299748ca3854e9915c750ff13df874
Miguel Angel Giulianetti Echeverri <magiudev@gmail.com>
2025-12-03 08:52:52 +0100

    connect to the db

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -7,6 +7,12 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
 });
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 app.UseCreateProductEndpoints();
