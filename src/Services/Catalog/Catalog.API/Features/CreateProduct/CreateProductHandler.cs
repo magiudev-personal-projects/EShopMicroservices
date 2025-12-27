756d3276e630fb9270567578a0620970a202fb84
Miguel Angel Giulianetti Echeverri <magiudev@gmail.com>
2025-12-27 17:25:10 +0100

    add missing log on create product handler

--- src/Services/Catalog/Catalog.API/Features/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Features/CreateProduct/CreateProductHandler.cs
@@ -1,9 +1,10 @@
 namespace Catalog.API.Features.CreateProduct;
 
-internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        logger.LogInformation("CreateProductCommandHandler.Handle called with {@command}", command);
         var product = CreateProductMaps.FromCommandToEntity(command);
         session.Store(product);
         await session.SaveChangesAsync(cancellationToken);
