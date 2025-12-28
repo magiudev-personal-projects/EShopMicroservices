ab9ca28d20b59d1ecabeaa20d67bda851f46993f
Miguel Angel Giulianetti Echeverri <magiudev@gmail.com>
2025-12-28 13:21:39 +0100

    define not found exception

---
+++ src/Services/Catalog/Catalog.API/Exeptions.cs
@@ -0,0 +1,8 @@
+using BuildingBlocks.NotFoundException;
+
+namespace Catalog.API.Exceptions;
+
+public class ProductNotFoundException: NotFoundException
+{
+    public ProductNotFoundException(Guid Id): base("Product", Id){}
+}
