3449dec83d020d5f036593055d454d306d21d505
Miguel Angel Giulianetti Echeverri <magiudev@gmail.com>
2025-12-28 17:43:56 +0100

    check if the product exist before deleting it

--- src/Services/Catalog/Catalog.API/Features/DeleteProduct/Hanlder.cs
+++ src/Services/Catalog/Catalog.API/Features/DeleteProduct/Hanlder.cs
@@ -1,5 +1,5 @@
 
-// using Catalog.API.Exceptions;
+using Catalog.API.Exceptions;
 
 namespace Catalog.API.Features.DeleteProduct;
 
@@ -7,9 +7,9 @@ public class Hanlder(IDocumentSession session): ICommandHandler<Command, Result>
 {
     public async Task<Result> Handle(Command command, CancellationToken cancellationToken)
     {
-        // var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
-        // if(product == null)
-        //     throw new ProductNotFoundException(command.Id);
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+        if(product == null)
+            throw new ProductNotFoundException(command.Id);
         session.Delete<Product>(command.Id);
         await session.SaveChangesAsync(cancellationToken);
         var result = new Result(true);
