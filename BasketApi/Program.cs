8d7ade23b21102d30702ae17be0e2f8f37f8a2b6
Miguel Angel Giulianetti Echeverri <magiudev@gmail.com>
2026-01-20 08:42:39 +0100

    add redis health check

--- BasketApi/Program.cs
+++ BasketApi/Program.cs
@@ -7,19 +7,22 @@ using HealthChecks.UI.Client;
 using Marten;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using FluentValidation;
-using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly;
+
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
     config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
 });
+
 builder.Services.AddValidatorsFromAssembly(assembly);
+
 var dbConnectionString = builder.Configuration.GetConnectionString("Database")!;
+
 builder.Services.AddMarten(options =>
 {
     options.Connection(dbConnectionString);
@@ -27,23 +30,27 @@ builder.Services.AddMarten(options =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IRepository, Repository>();
+
 builder.Services.Decorate<IRepository, CachedRepository>();
 
 builder.Services.AddExceptionHandler<ExceptionHandler>();
 
+var redisConnectionstring = builder.Configuration.GetConnectionString("Redis")!;
 builder.Services.AddHealthChecks()
-    .AddNpgSql(dbConnectionString);
-
-builder.Services.AddProblemDetails();
+    .AddNpgSql(dbConnectionString)
+    .AddRedis(redisConnectionstring);
 
 builder.Services.AddStackExchangeRedisCache(options =>
 {
-    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    options.Configuration = redisConnectionstring;
 });
+
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
 app.UseExceptionHandler();
+
 app.UseHealthChecks("/health",
     new HealthCheckOptions
     {
