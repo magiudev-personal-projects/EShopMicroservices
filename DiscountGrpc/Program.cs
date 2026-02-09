12b3fdbc638867902ed4c4a966d78cd90ede5f28
Miguel Angel Giulianetti Echeverri <magiudev@gmail.com>
2026-02-09 10:28:50 +0100

    rename cupons entity

--- DiscountGrpc/Program.cs
+++ DiscountGrpc/Program.cs
@@ -1,3 +1,5 @@
+using DiscountGrpc.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapGrpcService<DiscountService>();
 app.MapGet(
     "/",
     () =>
