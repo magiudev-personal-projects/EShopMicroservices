ae2b1a066050ae24793a3722c8fc454c62295a12
Miguel Angel Giulianetti Echeverri <magiudev@gmail.com>
2026-03-08 17:34:23 +0100

    define domain abstractions

---
+++ Ordering/Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,17 @@
+namespace Ordering.Domain.Abstractions;
+
+public abstract class Aggregate<T> : Entity<T>, IAggregate<T>
+{
+    public List<IDomainEvent> _domainEvents = new();
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    public void AddDomainEvent(IDomainEvent domainEvents)
+    {
+        _domainEvents.Add(domainEvents);
+    }
+
+    public IDomainEvent[] GetDomainEvents()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Ordering/Domain/Abstractions/Entity.cs
@@ -0,0 +1,10 @@
+namespace Ordering.Domain.Abstractions;
+
+public abstract class Entity<T> : IEntity<T>
+{
+    public T Id { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? ModifiedAt { get; set; }
+    public string? ModifiedBy { get; set; }
+}
