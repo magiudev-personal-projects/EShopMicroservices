8254f0c9ff7cdf76918a274102d2691fc42c653f
Miguel Angel Giulianetti Echeverri <magiudev@gmail.com>
2026-03-08 16:35:32 +0100

    add layer references

---
+++ Ordering/Application/DependencyInjection.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ Ordering/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,9 @@
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder AddServices(this WebApplicationBuilder app)
+    {
+        return app;
+    }
+}
