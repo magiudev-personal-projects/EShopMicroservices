d20e018cccf068393a0c2dfd0b80705855350cef
Miguel Angel Giulianetti Echeverri <magiudev@gmail.com>
2026-03-19 16:03:41 +0100

    define create order endpoint

---
+++ Ordering/Infrastructure/Routers/CreateOrder.cs
@@ -0,0 +1,40 @@
+using MediatR;
+using Ordering.Application.Dtos;
+using Ordering.Application.Features.CreateOrder;
+
+namespace Ordering.API.Endpoints;
+
+public record Request(OrderDto Order);
+public record Response(Guid Id);
+
+public static class Router
+{
+    public static void AddRoute(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("/orders", async (Request request, ISender sender) =>
+        {
+            var command = request.ToCommand();
+
+            var result = await sender.Send(command);
+
+            var response = result.ToResponse();
+
+            return Results.Created($"/orders/{response.Id}", response);
+        })
+        .WithName("CreateOrder")
+        .Produces<Response>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Order")
+        .WithDescription("Create Order");
+    }
+
+    public static CreateOrderCommand ToCommand(this Request request)
+    {
+        return new CreateOrderCommand(request.Order);
+    }
+
+    public static  Response ToResponse(this CreateOrderResult result)
+    {
+        return new Response(result.Id);
+    }
+}
