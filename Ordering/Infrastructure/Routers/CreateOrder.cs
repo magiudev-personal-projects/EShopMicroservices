fcc2ba6019c3a5a89216cef75c6eba2dff690b1c
Miguel Angel Giulianetti Echeverri <magiudev@gmail.com>
2026-03-20 17:04:25 +0100

    fix create order namespace

--- Ordering/Infrastructure/Routers/CreateOrder.cs
+++ Ordering/Infrastructure/Routers/CreateOrder.cs
@@ -2,30 +2,34 @@ using MediatR;
 using Ordering.Application.Dtos;
 using Ordering.Application.Features.CreateOrder;
 
-namespace Ordering.API.Endpoints;
+namespace Ordering.Infrastructure.Router.CreateOrder;
 
 public record Request(OrderDto Order);
+
 public record Response(Guid Id);
 
 public static class Router
 {
     public static void AddRoute(this IEndpointRouteBuilder app)
     {
-        app.MapPost("/orders", async (Request request, ISender sender) =>
-        {
-            var command = request.ToCommand();
+        app.MapPost(
+                "/orders",
+                async (Request request, ISender sender) =>
+                {
+                    var command = request.ToCommand();
 
-            var result = await sender.Send(command);
+                    var result = await sender.Send(command);
 
-            var response = result.ToResponse();
+                    var response = result.ToResponse();
 
-            return Results.Created($"/orders/{response.Id}", response);
-        })
-        .WithName("CreateOrder")
-        .Produces<Response>(StatusCodes.Status201Created)
-        .ProducesProblem(StatusCodes.Status400BadRequest)
-        .WithSummary("Create Order")
-        .WithDescription("Create Order");
+                    return Results.Created($"/orders/{response.Id}", response);
+                }
+            )
+            .WithName("CreateOrder")
+            .Produces<Response>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Order")
+            .WithDescription("Create Order");
     }
 
     public static CreateOrderCommand ToCommand(this Request request)
@@ -33,7 +37,7 @@ public static class Router
         return new CreateOrderCommand(request.Order);
     }
 
-    public static  Response ToResponse(this CreateOrderResult result)
+    public static Response ToResponse(this CreateOrderResult result)
     {
         return new Response(result.Id);
     }
