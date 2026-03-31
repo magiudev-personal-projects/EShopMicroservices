76a6bf203fcb78bf2e1b1ea422ed452b9ac7207f
Miguel Angel Giulianetti Echeverri <magiudev@gmail.com>
2026-03-31 16:09:11 +0200

    configure rate limit

--- YarpApiGateway/Program.cs
+++ YarpApiGateway/Program.cs
@@ -1,10 +1,26 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var reverseProxy = builder.Configuration.GetSection("ReverseProxy");
 
 builder.Services.AddReverseProxy().LoadFromConfig(reverseProxy);
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter(
+        "fixed",
+        opt =>
+        {
+            opt.PermitLimit = 5;
+            opt.Window = TimeSpan.FromSeconds(10);
+        }
+    );
+});
+
 var app = builder.Build();
+
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 
