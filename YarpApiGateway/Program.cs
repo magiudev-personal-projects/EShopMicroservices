4778f1162e41e9906c7959fe1177f77d760ee69b
Miguel Angel Giulianetti Echeverri <magiudev@gmail.com>
2026-03-31 15:22:23 +0200

    configure yarp

--- YarpApiGateway/Program.cs
+++ YarpApiGateway/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var reverseProxy = builder.Configuration.GetSection("ReverseProxy");
+
+builder.Services.AddReverseProxy().LoadFromConfig(reverseProxy);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapReverseProxy();
 
 app.Run();
